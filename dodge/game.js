9368893ba39f3208aed039477b40df8a4f431ea2
Christopher Geiger <christophergeiger3@gmail.com>
2016-11-17 20:58:20 +0000

    Added enemy movement and game over

--- dodge/game.js
+++ dodge/game.js
@@ -1,19 +1,44 @@
 var player;
 var enemy;
+var isGameOver;
 
 function setup(){
+    isGameOver = false;
     createCanvas(250,250);
     player = createSprite(width/2, height-25, 50, 50);
     enemy = createSprite(width/2, 0, 10, 30)
 }
 
 function draw(){
-    background(0,0,100);
-    drawSprites();
-    if((keyDown(RIGHT_ARROW) || keyDown(68)) && player.position.x < width-25){
-        player.position.x++;
+    if(isGameOver){
+        gameOver();
     }
-    if((keyDown(LEFT_ARROW) || keyDown(65)) && player.position.x > 25){
-        player.position.x--;
+    else{
+        
+        background(0,0,100);
+        drawSprites();
+        if(enemy.overlap(player)){
+            isGameOver = true;
+        }
+        
+        if((keyDown(RIGHT_ARROW) || keyDown(68)) && player.position.x < width-25){
+            player.position.x++;
+        }
+        if((keyDown(LEFT_ARROW) || keyDown(65)) && player.position.x > 25){
+            player.position.x--;
+        }
+        
+        enemy.position.y+=3;
+        if(enemy.position.y > height){
+            enemy.position.x = random(5, width-5);
+            enemy.position.y = 0;
+        }
     }
+}
+
+function gameOver(){
+    background(0);
+    textAlign(CENTER);
+    fill("white");
+    text("Game Over!", width/2, height/2);
 }
