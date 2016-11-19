a56a8af0935eab281f93721ac4f30c648baa32ed
Christopher Geiger <christophergeiger3@gmail.com>
2016-11-19 16:22:27 +0000

    Added images

--- dodge/game.js
+++ dodge/game.js
@@ -1,12 +1,24 @@
 var player;
+var playerImage;
 var enemy;
+var enemyImage;
 var isGameOver;
+var bgImg;
+
+function preload(){
+    bgImg = loadImage("christophergeiger.me/dodge/img/bg.png")
+    playerImage = loadImage("christophergeiger.me/dodge/img/player.png");
+    enemyImage = loadImage("christophergeiger.me/dodge/img/rock.png");
+}
 
 function setup(){
     isGameOver = false;
-    createCanvas(250,250);
-    player = createSprite(width/2, height-25, 50, 50);
+    createCanvas(256,256);
+    player = createSprite(width/2, height-(playerImage.height/2), 0, 0);
+    player.addImage(playerImage);
     enemy = createSprite(random(5, width-5), 0, 10, 30)
+    enemy.addImage(enemyImage);
+    enemy.rotationSpeed = 4.0;
 }
 
 function draw(){
@@ -15,16 +27,16 @@ function draw(){
     }
     else{
         
-        background(0,0,100);
+        background(bgImg);
         drawSprites();
         if(enemy.overlap(player)){
             isGameOver = true;
         }
         
-        if((keyDown(RIGHT_ARROW) || keyDown(68)) && player.position.x < width-25){
+        if((keyDown(RIGHT_ARROW) || keyDown(68)) && player.position.x < width-(playerImage.width/2)){
             player.position.x++;
         }
-        if((keyDown(LEFT_ARROW) || keyDown(65)) && player.position.x > 25){
+        if((keyDown(LEFT_ARROW) || keyDown(65)) && player.position.x > (playerImage.width)) {
             player.position.x--;
         }
         
@@ -32,6 +44,7 @@ function draw(){
         if(enemy.position.y > height){
             enemy.position.x = random(5, width-5);
             enemy.position.y = 0;
+            
         }
     }
 }
@@ -49,7 +62,7 @@ function mouseClicked(){
     {
         isGameOver = false;
         player.position.x = width/2;
-        player.position.y = height-25;
+        player.position.y = height-(playerImage.height/2);
         enemy.position.x = random(5, width-5);
         enemy.position.y = 0;
     }
