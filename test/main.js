a281caf974a12e8dd7bb4a7e398c8d5673b65f9f
Christopher Geiger <christophergeiger3@gmail.com>
2016-11-21 14:50:34 +0000

    Trying out thugify workshop

---
+++ test/main.js
@@ -0,0 +1,13 @@
+/*
+$('#go').onclick = function(){
+    var inputText = $('#text-input').val();
+    var thugifiedText = inputText + ', dawg';
+    $('#output').innerHTML = thugifiedText;
+};
+*/
+
+document.getElementById("go").onclick = function() {
+    var inputText = document.getElementById("text-input").val();
+    var thugifiedText = inputText + ", dawg";
+    document.getElementById("output").innerHTML = thugifiedText;
+};
