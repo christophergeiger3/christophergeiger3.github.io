4264236cd29b09328c48dfc82864bc49c1efd8d3
Christopher Geiger <christophergeiger3@gmail.com>
2016-11-21 15:15:04 +0000

    Finished Thugify workshop

--- test/main.js
+++ test/main.js
@@ -1,13 +1,6 @@
-/*
-$('#go').onclick = function(){
+
+document.getElementById('go').onclick = function(){
     var inputText = $('#text-input').val();
     var thugifiedText = inputText + ', dawg';
-    $('#output').innerHTML = thugifiedText;
-};
-*/
-
-document.getElementById('go').onclick = function() {
-    var inputText = $('#text-input').val();
-    var thugifiedText = inputText + ", dawg";
     document.getElementById('output').innerHTML = thugifiedText;
 };
