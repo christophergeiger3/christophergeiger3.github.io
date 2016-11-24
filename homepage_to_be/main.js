37140d1f8448c477220d37c4c054c67f69966cbb
Christopher Geiger <christophergeiger3@gmail.com>
2016-11-24 01:07:53 +0000

    Complete overhaul

--- homepage_to_be/main.js
+++ homepage_to_be/main.js
@@ -1,20 +1,117 @@
-var discard;
-var bgImg;
+// Inspired by https://p5js.org/examples/structure-setup-and-draw.html
 
-function setup(){
+var y = [100, 125, 300, 450, 600];
+var terminalBlink = false;
+var TERMINAL_GREEN = "#007f00";
+
+
+function setup() {
     createCanvas(windowWidth, windowHeight);
-    discard = false;
-    bgImg = loadImage('http://christophergeiger.me/homepage_to_be/img/bg_old_still.png');
+    stroke(255)
+    frameRate(30)
 }
 
 function draw(){
+    background(0);
+    terminal();
+    staticLines();
+}
+
+function terminal() {
+    
+    // Glitch effect
+    if(second()%2 == 0){
+        terminalBlink = !terminalBlink;
+    }
+    
+    if(!terminalBlink){
+        fill(color(TERMINAL_GREEN));
+        stroke(color(TERMINAL_GREEN));
+        rect(20, 10, 15, 30);
+    }
+    
+}
+
+function staticLines() {
+    y[0] -= 1.35;
+    y[1] -= 0.75;
+    y[2] -= 0.5;
+    y[3] -= 1.5;
+    y[4] -= 0.5;
+    
+    for(var x=0; x<y.length; x++)
+    {
+        if(y[x]<0){
+            y[x] = height;
+        }
+        
+        stroke(255);
+        fill(255);
+        line(0, y[x], width, y[x]);
+        
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+
+// Old Code...
+
+var discard;
+var bgImg;
+var bg;
+var thisCanvas;
+
+
+function setup(){
+    thisCanvas = createCanvas(100, 100); // windowWidth, windowHeight
+    discard = false;
+    bgImg = loadImage('http://christophergeiger.me/homepage_to_be/img/bg_old_still.png');
+    bg = createSprite(windowWidth,windowHeight,0,0);
+    bg.addImage(bgImg);
+    
+}
+
+function draw(){
+    
+    thisCanvas.position(mouseX, mouseY);
+    
+    
     if(!discard){
         cursor(HAND);
         background(bgImg);
         
-        cracks();
+        //cracks();
+        
     }
+    
 }
+
+function mousePressed(){
+    
+}
+
+
 
 function cracks(){
     
@@ -22,7 +119,11 @@ function cracks(){
     //line(0, height, width/2, height/2);
     
     for(var i=0; i<width; i++){
-        line(i, lineEquation(i), i+1, lineEquation(i+1))
+        
+        setTimeout(function() {
+            line(i, lineEquation(i), i+1, lineEquation(i+1));  
+        }, 0);
+        
         // passes a start and end point (x,f(x)) and (x+1,f(x+1)) to a line for every x on
         // the canvas
     }
@@ -42,6 +143,10 @@ function lineEquation(x){
     var crackSlope = 2;
     crackSlope = -crackSlope;
     //crackSlope -= random();
-    return crackSlope*x + height;
+    
+    return crackSlope*x + height; 
+    
     
 }
+
+*/
---
+++ test/main.js
@@ -0,0 +1,20 @@
+// Not my code...
+
+var myCanvas;
+
+function setup() {
+  //use a variable to store a pointer to the canvas
+  myCanvas = createCanvas(100, 100);
+}
+
+function draw() {
+  background(237, 34, 93);
+  fill(0);
+
+  //move the canvas to the horizontal mouse position
+  //relative to the window
+  myCanvas.position(winMouseX+1, windowHeight/2);
+
+ //the y of the square is relative to the canvas
+ rect(20,mouseY,60,60);
+}
