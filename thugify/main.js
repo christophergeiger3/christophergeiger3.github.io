7f82ec8ed09f5c3e03b7c3bd7f07484811463b0f
Christopher Geiger <christophergeiger3@gmail.com>
2019-03-08 23:44:54 -0500

    Point .github.io url to HackUConn repo

--- thugify/main.js
+++
@@ -1,6 +0,0 @@
-
-document.getElementById('go').onclick = function(){
-    var inputText = $('#text-input').val();
-    var thugifiedText = inputText + ', dawg';
-    document.getElementById('output').innerHTML = thugifiedText;
-};
